dfd31905e40c7a64d5d21c3ff03c429d9394f677
AlyonkaB <alyonkabagritskaya@gmail.com>
2024-08-25 20:03:08 +0300

    registration model Airport, AirplaneType, Airplane, Route, Crew, Flight, Order, Ticket for admin panel

--- system_for_tracking/admin.py
+++ system_for_tracking/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from system_for_tracking.models import (
+    Airport,
+    AirplaneType,
+    Airplane,
+    Route,
+    Crew,
+    Flight,
+    Order,
+    Ticket
+)
+
+
+admin.site.register(Airport)
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Route)
+admin.site.register(Crew)
+admin.site.register(Flight)
+admin.site.register(Order)
+admin.site.register(Ticket)

d4908306c371d689b05c00b4f34104bb7d9b0bbe
AlyonkaB <alyonkabagritskaya@gmail.com>
2024-10-21 10:54:39 +0300

    addeddrf_spectacular

--- Airport_api_service/settings.py
+++ Airport_api_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'system_for_tracking',
+    "drf_spectacular"
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- Airport_api_service/urls.py
+++ Airport_api_service/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("system_for_tracking.urls", namespace="system_for_tracking"))
+    path("", include("system_for_tracking.urls", namespace="system_for_tracking")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]

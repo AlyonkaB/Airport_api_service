a4d6fa058f3c1467dd9908c60a6a360536d42b6f
AlyonkaB <alyonkabagritskaya@gmail.com>
2024-10-21 10:57:24 +0300

    changing the case of the first letters in routes

--- system_for_tracking/urls.py
+++ system_for_tracking/urls.py
@@ -17,10 +17,10 @@ router.register("airport", AirportViewSet)
 router.register("airplane-type", AirplaneTypeViewSet)
 router.register("airplane", AirplaneViewSet)
 router.register("route", RouteViewSet)
-router.register("Crew", CrewViewSet)
-router.register("Flight", FlightViewSet)
-router.register("Order", OrderViewSet)
-router.register("Ticket", TicketViewSet)
+router.register("crew", CrewViewSet)
+router.register("flight", FlightViewSet)
+router.register("order", OrderViewSet)
+router.register("ticket", TicketViewSet)
 
 
 urlpatterns = [
